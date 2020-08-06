dc77dbe740d3cb2a57c8df3711aa1b2024480a79
cynthiaroma85 <68935046+cynthiaroma85@users.noreply.github.com>
2020-08-05 21:33:14 -0400

    Update ZombieAttack.java

--- src/utility/ZombieAttack.java
+++ src/utility/ZombieAttack.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class ZombieAttack {
 
-
+// coomit changes from github
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] inhabitants = new int[8];
